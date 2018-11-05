bfdc698cf1df05e61ce86884bd225eef9847ca96
may <lengdmei@126.com>
2018-11-05 10:45:35 +0800

    update

---
+++ SeleniumTest/SeleniumTest/AppData/StartRequest.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeleniumTest.AppData
+{
+    //这个类里开始写初始页面的进入，创建webdriver
+    class StartRequest
+    {
+        public IWebDriver driver = null;
+        public string _url;
+        public string Url
+        {
+            get { return _url; }
+            //设置一个默认的url
+            set {
+                if(value==" " || value==null)
+                {
+                    _url = "http://122.11.51.170:20002/server/login?sessionTimeOut=true";
+                }
+                else
+                {
+                    _url = value;
+                }
+                
+            }
+        }
+
+        //通过构造方法将初始url传入
+        public StartRequest(string url)
+        {
+            this._url = url;
+        }
+
+        //获得操作浏览器的driver
+        public void  MakeDriver(string browserType)
+        {
+           // IWebDriver driver = null;
+            // InternetExplorerDriver driver = new InternetExplorerDriver(@"C:\Users\bonree\Desktop\webapi\netframework\SeleniumTest\packages\Selenium.WebDriver.3.13.1");
+            if (browserType.Equals("IE"))
+            {
+                //使用IE时要取消浏览器的保护模式，添加如下两行代码。
+                InternetExplorerOptions options = new InternetExplorerOptions();
+                options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+                driver = new InternetExplorerDriver(options);
+            }
+            else if (browserType.Equals("Chrome".ToUpper()))
+            {
+                driver = new ChromeDriver();
+            }
+            else
+            {
+                driver = new FirefoxDriver();
+            }         
+
+        }
+
+        //进入请求页面
+        public IWebDriver RequestPage(string browserType)
+        {
+            MakeDriver(browserType);
+            driver.Navigate().GoToUrl(Url);
+            return driver;
+        }
+
+
+    }
+}
---
+++ SeleniumTest/SeleniumTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumTest.AppData;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+
+//本demo使用的是selenium2.0
+namespace SeleniumTest
+{
+    //后期把测试用例也放到文件夹中
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string banseUrl = "http://122.11.51.170:20002/server/login?sessionTimeOut=true";
+            StartRequest p = new StartRequest(banseUrl);
+
+            Console.WriteLine("请输入要使用的浏览器框架：ie,chrome,其他-Firefox");
+            string browserType = Console.ReadLine();
+            browserType = browserType.ToUpper();
+            IWebDriver driver = p.RequestPage(browserType);
+            //关闭浏览器
+            driver.Quit();
+
+
+
+
+            //ImageFormat.Png该格式selenium2.0才能用，3.0不能用
+            ITakesScreenshot screenshotDriver = driver as ITakesScreenshot;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            screenshot.SaveAsFile(@"\jietu.png", ImageFormat.Png);
+          //截图的路径必须是本地路径 \代表C盘根目录
+           
+        }
+    }
+}
